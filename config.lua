405c802eebdc1f6c4aa640f240739c84bcbe1aef
dapaath <dafaathaullah123@gmail.com>
2022-10-18 00:10:14 +0700

    new-update

--- config.lua
+++ config.lua
@@ -21,7 +21,7 @@ lvim.keys.normal_mode["<C-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<C-h>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["<Tab>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<C-Tab>"] = "<cmd>Telescope buffers<CR>"
-lvim.keys.normal_mode["<C-p>"] = "<cmd>lua require('lvim.core.telescope.custom-finders').find_project_files()<CR>"
+lvim.keys.normal_mode["<C-p>"] = "<cmd>Telescope find_files<CR>"
 lvim.keys.normal_mode["U"] = "<cmd>redo<CR>"
 lvim.keys.normal_mode["<C-.>"] = "<cmd>lua vim.lsp.buf.code_action()<CR>"
 lvim.keys.normal_mode["<C-/>"] = "<Plug>(comment_toggle_linewise_current)"
@@ -173,18 +173,14 @@ formatters.setup {
 -- -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  { command = "flake8", filetypes = { "python" } },
+  { command = "mypy", filetypes = { "python" } },
   {
     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
     command = "shellcheck",
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
     extra_args = { "--severity", "warning" },
   },
   {
     command = "codespell",
-    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    filetypes = { "javascript", "python" },
   },
 }
 
